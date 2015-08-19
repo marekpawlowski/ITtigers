ddc87ffa2f6b049510a404e3753b29ddd4a4c7e5
Tyngrysek <marek.pawlowski.pp@gmail.com>
2015-08-19 04:22:07 -0700

    4th commit MP

--- TigersApp/app/assets/javascripts/application.js
+++ TigersApp/app/assets/javascripts/application.js
@@ -16,4 +16,29 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 1000,
+    pause_on_hover: true,
+    animation_speed: 1000,
+    navigation_arrows: true,
+    bullets: false
+  }
+}); });
+$("#featured1").on("ready.fndtn.orbit", function(event) {
+  console.info("ready");
+});
+$("#featured1").on("before-slide-change.fndtn.orbit", function(event) {
+  console.info("before slide change");
+});
+$("#featured1").on("after-slide-change.fndtn.orbit", function(event, orbit) {
+  console.info("after slide change");
+  console.info("slide " + orbit.slide_number + " of " + orbit.total_slides);
+});
+$("#featured1").on("timer-started.fndtn.orbit", function(event, orbit) {
+  console.info("timer started");
+});
+$("#featured1").on("timer-stopped.fndtn.orbit", function(event, orbit) {
+  console.info("timer stopped");
+});
